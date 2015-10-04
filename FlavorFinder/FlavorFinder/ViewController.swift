bbc90b2b9dbca28735b4efd5f6ea5afdf1a5f8c8
Sudikoff Lab iMac <imac@dhcp-210-166.lab.cs.dartmouth.edu>
2015-10-04 14:14:04 -0400

    Initial Xcode project

---
+++ FlavorFinder/FlavorFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlavorFinder
+//
+//  Created by Sudikoff Lab iMac on 10/4/15.
+//  Copyright (c) 2015 TeamFive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
