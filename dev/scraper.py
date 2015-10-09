d46da4cd9ad97b8de7a79a4f122e2a9af1c64396
jon <imac@dhcp-210-209.lab.cs.dartmouth.edu>
2015-10-08 20:22:15 -0400

    modified scraper to create separate tables for matches and ingredients

--- dev/scraper.py
+++ dev/scraper.py
@@ -9,12 +9,16 @@ from bs4 import BeautifulSoup
 import sqlite3
 import re
 
-conn = sqlite3.connect('flavors.db')
+conn = sqlite3.connect('flavorbible.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE flavors
+c.execute('''CREATE TABLE matches
             (ingredient text, match_low text, match_medium text,
-            match_high text, match_holy text, season text, taste text, 
-            weight text, volume text, affinity text, quote text)''')
+            match_high text, match_holy text, affinity text,
+            quote text)''')
+
+c.execute('''CREATE TABLE ingredients
+            (ingredient text, season text, taste text, weight text,
+            volume text)''')
 
 book = epub.read_epub('flavorbible.epub')
 result = ""
@@ -41,25 +45,28 @@ for item in book.get_items():
                 
                 # season, taste, weight, volume
                 if s.text.startswith('Season:'):
-                    c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '%s', '', '', '', '', '')" % (itext.lower(), s.text[8:]))
+                    c.execute("INSERT INTO ingredients VALUES ('%s', '%s', '', '', '')" % (itext.lower(), s.text[8:]))
                 elif s.text.startswith('Taste:'):
-                    c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '', '%s', '', '', '', '')" % (itext.lower(), s.text[7:]))
+                    #c.execute("INSERT INTO ingredients VALUES ('%s', '', '%s', '', '')" % (itext.lower(), s.text[7:]))
+                    c.execute("UPDATE ingredients SET taste='%s' WHERE ingredient='%s'" % (s.text[7:], itext.lower()))
                 elif s.text.startswith('Weight:'):
-                    c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '', '', '%s', '', '', '')" % (itext.lower(), s.text[8:]))
+                    #c.execute("INSERT INTO ingredients VALUES ('%s', '', '', '%s', '')" % (itext.lower(), s.text[8:]))
+                    c.execute("UPDATE ingredients SET weight='%s' WHERE ingredient='%s'" % (s.text[8:], itext.lower()))
                 elif s.text.startswith('Volume:'):
-                    c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '', '', '', '%s', '', '')" % (itext.lower(), s.text[8:]))
+                    #c.execute("INSERT INTO ingredients VALUES ('%s', '', '', '', '%s')" % (itext.lower(), s.text[8:]))
+                    c.execute("UPDATE ingredients SET volume='%s' WHERE ingredient='%s'" % (s.text[8:], itext.lower()))
 
                 # quote
                 elif s['class'] == ['p1']:
                     quote = s.text
                     s = s.find_next_sibling()
                     quote += ' ' + s.text
-                    c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '', '', '', '', '', '%s')" % (itext.lower(), quote))
+                    c.execute("INSERT INTO matches VALUES ('%s', '', '', '', '', '', '%s')" % (itext.lower(), quote))
                 # flavor affinities
                 elif s['class'] == ['h4']:
                     s = s.find_next_sibling()
                     while s != None:
-                        c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '', '', '', '', '', '%s', '')" % (itext.lower(), s.text))
+                        c.execute("INSERT INTO matches VALUES ('%s', '', '', '', '', '%s', '')" % (itext.lower(), s.text))
 
                         try:
                             if s.find_next_sibling()['class'] in (['lh'], ['lh1'], ['p1']):
@@ -69,17 +76,17 @@ for item in book.get_items():
                         s = s.find_next_sibling()
                 # match-low
                 elif s.find('strong') == None:
-                    c.execute("INSERT INTO flavors VALUES ('%s', '%s', '', '', '', '', '', '', '', '', '')" % (itext.lower(), s.text.lower()))
+                    c.execute("INSERT INTO matches VALUES ('%s', '%s', '', '', '', '', '')" % (itext.lower(), s.text.lower()))
                 else:
                     # match-medium
                     if s.text.islower():
-                        c.execute("INSERT INTO flavors VALUES ('%s', '', '%s', '', '', '', '', '', '', '', '')" % (itext.lower(), s.text.lower()))
+                        c.execute("INSERT INTO matches VALUES ('%s', '', '%s', '', '', '', '')" % (itext.lower(), s.text.lower()))
                     # match-high
                     elif s.text.isupper() and s.text.startswith('*') == False:
-                        c.execute("INSERT INTO flavors VALUES ('%s', '', '', '%s', '', '', '', '', '', '', '')" % (itext.lower(), s.text.lower()))
+                        c.execute("INSERT INTO matches VALUES ('%s', '', '', '%s', '', '', '')" % (itext.lower(), s.text.lower()))
                     # match-holy
                     elif s.text.isupper() and s.text.startswith('*') == True:
-                        c.execute("INSERT INTO flavors VALUES ('%s', '', '', '', '%s', '', '', '', '', '', '')" % (itext.lower(), s.text.lower()[1:]))
+                        c.execute("INSERT INTO matches VALUES ('%s', '', '', '', '%s', '', '')" % (itext.lower(), s.text.lower()[1:]))
 
                 s = s.find_next_sibling()
             print('')
