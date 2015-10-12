7e2b805159e1fa05138c2a9724d692f78530438b
jon <imac@dhcp-210-77.lab.cs.dartmouth.edu>
2015-10-11 23:40:38 -0400

    Add: backward and forward navigation buttons.

--- FlavorFinder/FlavorFinder/MatchTableViewController.swift
+++ FlavorFinder/FlavorFinder/MatchTableViewController.swift
@@ -9,13 +9,41 @@
 import UIKit
 import SQLite
 
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element? {
+        if items.isEmpty {
+            return nil
+        } else {
+            return items.removeLast()
+        }
+    }
+    mutating func removeAll() {
+        items.removeAll()
+    }
+    mutating func isEmpty() -> Bool {
+        return items.isEmpty
+    }
+}
+
 class MatchTableViewController: UITableViewController, UITableViewDelegate, UISearchBarDelegate {
     // MARK: Properties
-    
     @IBOutlet weak var searchBar: UISearchBar!
     
+    
+    // MARK: Actions
+    @IBOutlet weak var goBackBtn: UIBarButtonItem!
+    @IBOutlet weak var goForwardBtn: UIBarButtonItem!
+    
+    // GLOBALS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // -------
     var matchesTable: Query!
     var ingredientsTable: Query!
+    
+    // Constants for schema strings.
     let SCHEMA_TABLE_INGREDIENTS = "ingredients"
     let SCHEMA_TABLE_MATCHES = "matches"
     let SCHEMA_COL_ID = Expression<Int>("id")
@@ -23,33 +51,60 @@ class MatchTableViewController: UITableViewController, UITableViewDelegate, UISe
     let SCHEMA_COL_MATCHLEVEL = Expression<Int>("match_level")
     let SCHEMA_COL_NAME = Expression<String>("name")
 
+    // Array of all ingredients from database.
     var allIngredients = [Ingredient]()
+    
+    // Array of all cells that CAN be displayed.
     var allCells = [Ingredient]()
+    
+    // Array of all cells that ARE displayed (filtered version of 'allCells').
     var filteredCells = [Ingredient]()
     
-//    var ingredientsSearchController = UISearchController()
-
+    // This boolean will activate the colored backgrounds.
+    // We only want to show the colors when viewing matches, not the list of all ingredients.
     var viewingMatches = false
+    
+    var currentIngredient : Ingredient?
+    
+    // Used for going backward.
+    var history = Stack<Ingredient?>()
+    
+    // Used for going forward.
+    var future = Stack<Ingredient?>()
+    // ---------------------------------------------------------
+    
+    
+    @IBAction func goBack(sender: UIBarButtonItem) {
+        if let curr = currentIngredient {
+            if let pastIngredient = history.pop() {
+                showIngredient(pastIngredient!)
+            } else {
+                showAllIngredients()
+            }
+            future.push(curr)
+        }
+    }
+    
+    @IBAction func goForward(sender: UIBarButtonItem) {
+        if let futureIngredient = future.pop() {
+            if let curr = currentIngredient {
+                history.push(curr)
+            }
+            showIngredient(futureIngredient!)
+        }
+    }
+    
+    func pushToHistory() {
+        future.removeAll()
+        if let curr = currentIngredient {
+            history.push(curr)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loadMatches()
-        
-//        // Configure ingredientSearchController
-//        self.ingredientsSearchController = ({
-//            // Two setups provided below:
-//            
-//            // Setup One: This setup present the results in the current view.
-//            let controller = UISearchController(searchResultsController: nil)
-////            controller.searchResultsUpdater = self.ingredientsSearchController
-//            controller.hidesNavigationBarDuringPresentation = false
-//            controller.dimsBackgroundDuringPresentation = false
-//            controller.searchBar.searchBarStyle = .Minimal
-//            controller.searchBar.sizeToFit()
-//            self.tableView.tableHeaderView = controller.searchBar
-//            return controller
-//        })()
+        print("hello")
+        loadIngredients()
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -58,10 +113,51 @@ class MatchTableViewController: UITableViewController, UITableViewDelegate, UISe
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    func loadMatches() {
+    func showAllIngredients() {
+        allCells = allIngredients
+        filteredCells = allIngredients
+        self.title = "All Ingredients"
+        currentIngredient = nil
+        
+        self.tableView.reloadData()
+    }
+    
+    func showIngredient(ingredient: Ingredient) {
+        currentIngredient = ingredient
+        
+        // Set navigation title to ingredient's name.
+        self.title = ingredient.name
+        
+        // Get all the match ids of matches for this ingredient.
+        let matches = matchesTable.filter(SCHEMA_COL_ID == ingredient.id)
+        
+        // Reset 'allCells' with the ingredients that have those match ids.
+        allCells.removeAll()
+        for m in matches {
+            if let found = find(lazy(allIngredients).map({ $0.id == m[self.SCHEMA_COL_MATCHID] }), true) {
+                allIngredients[found].matchLevel = m[SCHEMA_COL_MATCHLEVEL]
+                allCells.append(allIngredients[found])
+            }
+        }
+        
+        // Reset 'filteredCells' with new matches.
+        filteredCells = allCells
+        
+        // This boolean will activate the colored backgrounds.
+        // We only want to show the colors when viewing matches, not the list of all ingredients.
+        viewingMatches = true
+        
+        // Show the new ingredients on our table.
+        self.tableView.reloadData()
+        
+        //        self.performSegueWithIdentifier("yourIdentifier", sender: self)
+    }
+    
+    func loadIngredients() {
         let dbpath = NSBundle.mainBundle().pathForResource("flavorbible", ofType: "db")
         let db = Database(dbpath!)
         
+        // Fetch our ingredient
         ingredientsTable = db[SCHEMA_TABLE_INGREDIENTS]
         matchesTable = db[SCHEMA_TABLE_MATCHES];
         
@@ -76,8 +172,7 @@ class MatchTableViewController: UITableViewController, UITableViewDelegate, UISe
             }
         }
         
-        allCells = allIngredients
-        filteredCells = allIngredients
+        showAllIngredients()
     }
 
     override func didReceiveMemoryWarning() {
@@ -104,18 +199,26 @@ class MatchTableViewController: UITableViewController, UITableViewDelegate, UISe
 
         // Fetches the appropriate ingredient to display.
         let ingredient = filteredCells[indexPath.row]
+        
+        // Set's the cell label to the ingredient's name.
         cell.nameLabel.text = ingredient.name
         
         if viewingMatches {
             switch ingredient.matchLevel {
+                
+            // Match: low. White.
             case 1:
                 cell.backgroundColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: CGFloat(0.3))
+            // Match: medium. Yellow.
             case 2:
                 cell.backgroundColor = UIColor(red: 255/255.0, green: 237/255.0, blue: 105/255.0, alpha: CGFloat(0.3))
+            // Match: high. Blue.
             case 3:
                 cell.backgroundColor = UIColor(red: 105/255.0, green: 230/255.0, blue: 255/255.0, alpha: CGFloat(0.3))
+            // Match: greatest. Green.
             case 4:
                 cell.backgroundColor = UIColor(red: 105/255.0, green: 255/255.0, blue: 150/255.0, alpha: CGFloat(0.3))
+            // Default. White.
             default:
                 cell.backgroundColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: CGFloat(0.3))
             }
@@ -124,24 +227,12 @@ class MatchTableViewController: UITableViewController, UITableViewDelegate, UISe
     }
     
     override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        pushToHistory()
+        
+        // Get tapped ingredient.
         let ingredient = filteredCells[indexPath.row]
         
-        let matches = matchesTable.filter(SCHEMA_COL_ID == ingredient.id)
-        
-        allCells.removeAll()
-        
-        for m in matches {
-            if let found = find(lazy(allIngredients).map({ $0.id == m[self.SCHEMA_COL_MATCHID] }), true) {
-                allIngredients[found].matchLevel = m[SCHEMA_COL_MATCHLEVEL]
-                allCells.append(allIngredients[found])
-            }
-        }
-        
-        filteredCells = allCells
-        viewingMatches = true
-        
-        self.tableView.reloadData()
-        //        self.performSegueWithIdentifier("yourIdentifier", sender: self)
+        showIngredient(ingredient)
     }
     
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
