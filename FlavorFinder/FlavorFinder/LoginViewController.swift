5936faa2a06978db07f4b1529133d7ef980b6f3c
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2015-10-22 22:19:41 -0400

    create basic login view; no functionality

---
+++ FlavorFinder/FlavorFinder/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  FlavorFinder
+//
+//  Created by Courtney Ligh on 10/22/15.
+//  Copyright © 2015 TeamFive. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginUserTextField: UITextField!
+    @IBOutlet weak var loginPassTextField: UITextField!
+    
+    
+    // MARK: Actions
+    @IBAction func goTableViewTEMP(sender: UIButton) {
+    }
+    
+    @IBAction func loginActionBtn(sender: UIButton) {
+    }
+    
+    
+}
