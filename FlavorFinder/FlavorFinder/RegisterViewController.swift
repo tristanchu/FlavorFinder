18555b8314302b2e82942021e619b45f1fe0226e
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2015-10-24 17:51:46 -0400

    Create and connect register page to login

---
+++ FlavorFinder/FlavorFinder/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  FlavorFinder
+//
+//  Created by Courtney Ligh on 10/24/15.
+//  Copyright © 2015 TeamFive. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class RegisterViewController: UIViewController {
+    
+    
+    
+}
