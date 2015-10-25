9950c4a8ac4bcc1067d9c7f0fb90a483373f498a
jaki1234 <blumoon729@MacBook-Pro-33.local>
2015-10-25 15:56:32 -0400

    register view func breakdown and basic UI improved

--- FlavorFinder/FlavorFinder/RegisterViewController.swift
+++ FlavorFinder/FlavorFinder/RegisterViewController.swift
@@ -12,19 +12,65 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
-    @IBOutlet weak var RegisterLabel: UILabel!
-    @IBOutlet weak var RegisterEmail: UITextField!
-    @IBOutlet weak var RegisterPassword: UITextField!
-    @IBOutlet weak var RegisterCOPPA: UILabel!
-    @IBOutlet weak var RegisterSubmitBtn: UIButton!
     //// somewhere we need a privacy policy :)
     
-    func requestNewUser() {
+    // MARK: Properties -----------------------------------------------
+    @IBOutlet weak var registerLabel: UILabel!
+    @IBOutlet weak var registerEmail: UITextField!
+    @IBOutlet weak var registerUsername: UITextField!
+    @IBOutlet weak var registerPassword: UITextField!
+    @IBOutlet weak var registerCOPPA: UILabel!
+    
+    // MARK: Segue Identifiers ----------------------------------------
+    /// eventually segue to logged in!
+
+    // MARK: Actions --------------------------------------------------
+
+    @IBAction func registerSubmitActionBtn(sender: UIButton) {
+        if (registerEmail.text != nil && registerUsername.text != nil && registerPassword.text != nil) {
+            requestNewUser(registerEmail.text!, username: registerUsername.text!, pw: registerPassword.text!);
+        }
+    }
+    
+    // REGISTER FUNCTIONS ---------------------------------------------
+    func requestNewUser(email: String, username: String, pw: String) {
+        if (hasInvalidEmail(email)) {
+            return;
+        }
+        if (hasInvalidUsername(username)) {
+            return;
+        }
+        if (hasInvalidPassword(pw)) {
+            return;
+        }
+        
      /// here, send new parse request for new user credentials
     }
     
     func registerSuccess() {
         //// success!
+    }
+    
+    // VALIDATION FUNCTIONS ------------------------------------------
+    func hasInvalidEmail(email: String) -> Bool {
+        if (email.isEmpty) {
+            return true;
+        }
+        return false;
+    }
+    
+    func hasInvalidUsername(username: String) -> Bool {
+        if (username.isEmpty) {
+            return true;
+        }
+        return false;
+    }
+    
+    func hasInvalidPassword(pw: String) -> Bool {
+        if (pw.isEmpty) {
+            return true;
+        }
+        return false;
     }
     
 }
