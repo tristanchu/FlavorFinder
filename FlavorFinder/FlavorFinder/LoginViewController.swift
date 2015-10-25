fb52a09f41d0eb7b703a3c2004b0d836af27a787
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2015-10-25 14:00:31 -0400

    Login button checks for fields. no Parse usage yet.

--- FlavorFinder/FlavorFinder/LoginViewController.swift
+++ FlavorFinder/FlavorFinder/LoginViewController.swift
@@ -11,25 +11,43 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    // MARK: Properties
+    // MARK: Properties -----------------------------------------------
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var loginUserTextField: UITextField!
     @IBOutlet weak var loginPassTextField: UITextField!
     
     
-    // MARK: Actions
+    // MARK: Segue Identifiers ----------------------------------------
+    let loginToRegister = "segueLoginToRegister"
+    let loginToMatchTable = "segueLoginToMatchTable"
+
+
+    // MARK: Actions --------------------------------------------------
     @IBAction func loginActionBtn(sender: UIButton) {
+        loginUser(loginUserTextField, pwField: loginPassTextField)
     }
     
-    
+    // Send user to RegisterViewController page.
     @IBAction func goRegisterActionBtn(sender: UIButton) {
-        self.performSegueWithIdentifier("segueLoginToRegister", sender: self)
+        self.performSegueWithIdentifier(loginToRegister, sender: self)
     }
     
+    // Temporary button to go to main page without logging in
     @IBAction func goTableViewTEMP(sender: UIButton) {
-        self.performSegueWithIdentifier("segueLoginToMatchTable", sender: self)
+        self.performSegueWithIdentifier(loginToMatchTable, sender: self)
     }
     
     
+    // LOGIN FUNCTIONS -------------------------------------------------
     
+    func loginUser(usernameField: UITextField!, pwField: UITextField!) {
+        if usernameField.text != "" && pwField.text != "" {
+            // Fields not empty; check if valid user
+            self.loginLabel.text = "Tried to login"
+            // Change to PFUser.logInWithUsernameInBackground
+        } else {
+            // Empty Fields:
+            self.loginLabel.text = "Must enter both fields to Login"
+        }
+    }
 }
