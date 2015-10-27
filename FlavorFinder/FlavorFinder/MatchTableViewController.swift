def98ac90516f233903e65acce8bcfac78b1551d
jon <imac@dhcp-210-77.lab.cs.dartmouth.edu>
2015-10-26 20:10:10 -0400

    Separate Stack to its own file.

--- FlavorFinder/FlavorFinder/MatchTableViewController.swift
+++ FlavorFinder/FlavorFinder/MatchTableViewController.swift
@@ -12,27 +12,6 @@ import Parse
 import FontAwesome_swift
 import Darwin
 
-struct Stack<Element> {
-    var items = [Element]()
-    mutating func push(item: Element) {
-        items.append(item)
-    }
-    mutating func pop() -> Element? {
-        if items.isEmpty {
-            return nil
-        } else {
-            return items.removeLast()
-        }
-    }
-    mutating func removeAll() {
-        items.removeAll()
-    }
-    mutating func isEmpty() -> Bool {
-        return items.isEmpty
-    }
-}
-
-
 class MatchTableViewController: UITableViewController, UISearchBarDelegate {
     // GLOBALS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // -------
