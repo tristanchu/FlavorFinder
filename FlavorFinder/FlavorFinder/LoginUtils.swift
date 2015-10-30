a109207164b1217c71332f49a5356fe9f37a955a
jaki1234 <blumoon729@MacBook-Pro-33.local>
2015-10-29 20:23:08 -0400

    more registration validation

--- FlavorFinder/FlavorFinder/LoginUtils.swift
+++ FlavorFinder/FlavorFinder/LoginUtils.swift
@@ -9,10 +9,10 @@
 import Foundation
 import UIKit
 
-var USERNAME_CHAR_MAX = 50;
-var USERNAME_CHAR_MIN = 1;
-var PASSWORD_CHAR_MIN = 6;
-var PASSWORD_CHAR_MAX = 50;
+var USERNAME_CHAR_MAX = 50
+var USERNAME_CHAR_MIN = 1
+var PASSWORD_CHAR_MIN = 6
+var PASSWORD_CHAR_MAX = 50
 
 var backgroundColor_normal: UIColor!
 var backgroundColor_error: UIColor = UIColor(red: 250/255.0, green: 126/255.0, blue: 107/255.0, alpha: 0.5)
@@ -26,9 +26,12 @@ func isInvalidEmail(email:String) -> Bool {
 }
 
 func isInvalidUsername(username: String) -> Bool {
-    return username.isEmpty
+    return (username.isEmpty ||
+        username.characters.count > USERNAME_CHAR_MAX ||
+        username.characters.count < USERNAME_CHAR_MIN)
 }
 
 func isInvalidPassword(password: String) -> Bool {
-    return password.isEmpty
-}
+    return (password.isEmpty ||
+        password.characters.count > PASSWORD_CHAR_MAX ||
+        password.characters.count < PASSWORD_CHAR_MIN)}
