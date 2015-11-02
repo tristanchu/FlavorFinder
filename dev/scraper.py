1de0988ce2c96f72ff9b6499fe3187b68b59b2e7
jon <imac@dhcp-210-77.lab.cs.dartmouth.edu>
2015-11-01 19:29:49 -0500

    Scraper can now update match Ids to use parse Object Ids.
    Run 'scraper.py update' to do this.
    
    Matches and Ingredients are now on Parse.

--- dev/scraper.py
+++ dev/scraper.py
@@ -5,52 +5,44 @@ Scraper for the 'The Flavor Bible'.
 Created by Jon.
 """
 
-import sqlite3
-import re
 import json
 import os
+import re
+import sqlite3
+import sys
 
 import ebooklib
 from ebooklib import epub
 from bs4 import BeautifulSoup
 
 
-if os.path.isfile("flavorbible.db"):
-    os.remove("flavorbible.db")
-if os.path.isfile("ingredients.json"):
-    os.remove("ingredients.json")
-if os.path.isfile("matches.json"):
-    os.remove("matches.json")
-
-conn = sqlite3.connect('flavorbible.db')
-c = conn.cursor()
-
-c.execute('''CREATE TABLE ingredients(
-            id int PRIMARY KEY,
-            name text UNIQUE, 
-            season text,
-            taste text,
-            weight text,
-            volume text,
-            vegetarian int,
-            dairy int,
-            kosher int,
-            nuts int
-          )''')
-
-c.execute('''CREATE TABLE matches(
-            id int,
-            match_id int,
-            match_level int,
-            upvotes int,
-            downvotes int,
-            affinity text,
-            quote text,
-            PRIMARY KEY(id, match_id)
-          )''')
-
-
 latest_id = 0
+
+
+def createTables(c):
+    c.execute('''CREATE TABLE ingredients(
+                id int PRIMARY KEY,
+                name text UNIQUE, 
+                season text,
+                taste text,
+                weight text,
+                volume text,
+                vegetarian int,
+                dairy int,
+                kosher int,
+                nuts int
+              )''')
+
+    c.execute('''CREATE TABLE matches(
+                id int,
+                match_id int,
+                match_level int,
+                upvotes int,
+                downvotes int,
+                affinity text,
+                quote text,
+                PRIMARY KEY(id, match_id)
+              )''')
 
 def fixName(s):
     s = s.lower()
@@ -127,6 +119,11 @@ def addMatches(ingredients, matches, ingredient_ids, id, i, s, match_level):
         }
 
 
+def removeExistingFiles(files):
+    for f in files:
+        if os.path.isfile(f):
+            os.remove(f)
+
 def writeResultsToJSON(filename, data):
     results = {}
     results["results"] = []
@@ -136,158 +133,186 @@ def writeResultsToJSON(filename, data):
     with open(filename, 'w') as jsonfile:
         json.dump(results, jsonfile, indent=4)
 
+def writeUpdatesToJSON(filename, data):
+    results = {}
+    results["results"] = []
+    for item in data:
+        results["results"].append(item)
+    with open(filename, 'w') as jsonfile:
+        json.dump(results, jsonfile, indent=4)
 
-def writeIngredientsToTable(data):
+def loadResultsFromJSON(filename):
+    with open(filename) as file:
+        results = json.load(file)
+        return results["results"]
+
+def writeIngredientsToTable(c, data):
     for key in data:
         row = data[key]
         c.execute("INSERT INTO ingredients VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (row["tmp_id"], row["name"], row["season"], row["taste"], row["weight"], row["volume"], row["vegetarian"], row["dairy"], row["kosher"], row["nuts"]))
 
-def writeMatchesToTable(data):
+def writeMatchesToTable(c, data):
     for key in data:
         row = data[key]
         c.execute("INSERT INTO matches VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (row["ingredient_id"], row["match_id"], row["match_level"], row["upvotes"], row["downvotes"], row["affinity"], row["quote"]))
 
+def updateMatchesToUseParseIds(matches, parseIngredients):
+    for match in matches:
+        i_ingredient = next( (i for i in parseIngredients if i["tmp_id"] == match["ingredient_id"]) )
+        m_ingredient = next( (i for i in parseIngredients if i["tmp_id"] == match["match_id"]) )
+        match["ingredient_id"] = i_ingredient["objectId"]
+        match["match_id"] = m_ingredient["objectId"]
 
 
 if __name__ == '__main__':
-    global latest_id
-    ingredients_fieldnames = ['tmp_id', 'name', 'season', 'taste', 'weight', 'volume', 'vegetarian', 'dairy', 'kosher', 'nuts']
-    matches_fieldnames = ['ingredient_id', 'match_id', 'match_level', 'upvotes', 'downvotes', 'affinity', 'quote']
-
-    ingredients = {}
-    matches = {}
     
-    ingredient_ids = {}
+    if len(sys.argv) > 1 and sys.argv[1] == "update":
+        parseIngredients = loadResultsFromJSON('Ingredient.json')
+        matches = loadResultsFromJSON('matches.json')
+        updateMatchesToUseParseIds(matches, parseIngredients)
+        writeUpdatesToJSON('Match.json', matches)
+    else:
+        global latest_id
 
-    book = epub.read_epub('flavorbible.epub')
-    result = ""
-    for item in book.get_items():
-        type = item.get_type()
-        if type == ebooklib.ITEM_DOCUMENT:
-            soup = BeautifulSoup(item.content, 'lxml')
-    
-            # Find ingredient listings.
-            for ingredient in soup.find_all('p', {'class' : ['lh', 'lh1']}):
-                print('HEADING: ', ingredient)
+        ingredients = {}
+        matches = {}
+        ingredient_ids = {}
+        
+        ingredients_fieldnames = ['tmp_id', 'name', 'season', 'taste', 'weight', 'volume', 'vegetarian', 'dairy', 'kosher', 'nuts']
+        matches_fieldnames = ['ingredient_id', 'match_id', 'match_level', 'upvotes', 'downvotes', 'affinity', 'quote']
 
-                i = fixName(ingredient.text)
-            
-                if i in ingredient_ids:
-                    id = ingredient_ids[i]
-                else:
-                    id = latest_id
-                    ingredients[id] = {
-                        "tmp_id": id,
-                        "name": i,
-                        "season": '',
-                        "taste": '',
-                        "weight": '',
-                        "volume": '',
-                        "vegetarian":  '1',
-                        "dairy": '0',
-                        "kosher": '0',
-                        "nuts": '0'
-                    }
-                    ingredient_ids[i] = id
-                    latest_id += 1
-            
-            
-                # Find what goes well with these ingredients.
-                sibling = ingredient.find_next_sibling()
-            
-                while sibling != None:
-                    s = sibling.text
-                    try:
-                        if sibling['class'] in (['lh'], ['lh1']):
-                            break
-                    except:
-                        break
-                    print('content: ', sibling)
-                    
-                    # season, taste, weight, volume
-                    if s.startswith('Season:'):
-                        ingredients[id]["season"] = s[8:]
-                    elif s.startswith('Taste:'):
-                        ingredients[id]["taste"] = s[7:]
-                    elif s.startswith('Weight:'):
-                        ingredients[id]["weight"] = s[8:]
-                    elif s.startswith('Volume:'):
-                        ingredients[id]["volume"] = s[8:]
-                    elif s.startswith('Tips'):
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['ul3']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-            
-                    # quote
-                    elif sibling['class'] == ['p1']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-#                        quote = s
-#                        sibling = sibling.find_next_sibling()
-#                        s = sibling.text
-#                        quote += ' ' + s
-                        #c.execute("INSERT INTO matches VALUES ('%s', '', '', '', '%s')" % (i, quote))
-                    elif sibling['class'] == ['ca'] or sibling['class'] == ['ca3']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['img']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['boxh']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['ext']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['exts']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['bl'] or sibling['class'] == ['bl1'] or sibling['class'] == ['bl3']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    elif sibling['class'] == ['sbh'] or sibling['class'] == ['sbtx1']:
-                        sibling = sibling.find_next_sibling()
-                        continue
-                    # flavor affinities
-                    elif sibling['class'] == ['h4']:
-                        sibling = sibling.find_next_sibling()
-                        while sibling != None:
-                            #c.execute("INSERT INTO matches VALUES ('%s', '', '', '%s', '')" % (i, s))
-                            try:
-                                if sibling.find_next_sibling()['class'] in (['lh'], ['lh1'], ['p1']):
-                                    break
-                            except:
-                                break
-                            sibling = sibling.find_next_sibling()
-            
-                    # match-low
-                    elif sibling.find('strong') == None:
-                        addMatches(ingredients, matches, ingredient_ids, id, i, s, 1)
+        removeExistingFiles(['flavorbible.db', 'ingredients.json', 'matches.json'])
+        conn = sqlite3.connect('flavorbible.db')
+        c = conn.cursor()
+        createTables(c)
+        
+        book = epub.read_epub('flavorbible.epub')
+        result = ""
+        for item in book.get_items():
+            type = item.get_type()
+            if type == ebooklib.ITEM_DOCUMENT:
+                soup = BeautifulSoup(item.content, 'lxml')
+        
+                # Find ingredient listings.
+                for ingredient in soup.find_all('p', {'class' : ['lh', 'lh1']}):
+                    print('HEADING: ', ingredient)
+
+                    i = fixName(ingredient.text)
+                
+                    if i in ingredient_ids:
+                        id = ingredient_ids[i]
                     else:
-                        # match-medium
-                        if s.islower():
-                            addMatches(ingredients, matches, ingredient_ids, id, i, s, 2)
-                        # match-high
-                        elif s.isupper() and s.startswith('*') == False:
-                            addMatches(ingredients, matches, ingredient_ids, id, i, s, 3)
-                        # match-holy
-                        elif s.isupper() and s.startswith('*') == True:
-                            addMatches(ingredients, matches, ingredient_ids, id, i, s[1:], 4)
-            
-                    sibling = sibling.find_next_sibling()
-    
-    
-                print('')
-    
+                        id = latest_id
+                        ingredients[id] = {
+                            "tmp_id": id,
+                            "name": i,
+                            "season": '',
+                            "taste": '',
+                            "weight": '',
+                            "volume": '',
+                            "vegetarian":  1,
+                            "dairy": 0,
+                            "kosher": 0,
+                            "nuts": 0
+                        }
+                        ingredient_ids[i] = id
+                        latest_id += 1
+                
+                
+                    # Find what goes well with these ingredients.
+                    sibling = ingredient.find_next_sibling()
+                
+                    while sibling != None:
+                        s = sibling.text
+                        try:
+                            if sibling['class'] in (['lh'], ['lh1']):
+                                break
+                        except:
+                            break
+                        print('content: ', sibling)
+                        
+                        # season, taste, weight, volume
+                        if s.startswith('Season:'):
+                            ingredients[id]["season"] = s[8:]
+                        elif s.startswith('Taste:'):
+                            ingredients[id]["taste"] = s[7:]
+                        elif s.startswith('Weight:'):
+                            ingredients[id]["weight"] = s[8:]
+                        elif s.startswith('Volume:'):
+                            ingredients[id]["volume"] = s[8:]
+                        elif s.startswith('Tips'):
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['ul3']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                
+                        # quote
+                        elif sibling['class'] == ['p1']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+    #                        quote = s
+    #                        sibling = sibling.find_next_sibling()
+    #                        s = sibling.text
+    #                        quote += ' ' + s
+                            #c.execute("INSERT INTO matches VALUES ('%s', '', '', '', '%s')" % (i, quote))
+                        elif sibling['class'] == ['ca'] or sibling['class'] == ['ca3']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['img']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['boxh']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['ext']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['exts']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['bl'] or sibling['class'] == ['bl1'] or sibling['class'] == ['bl3']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        elif sibling['class'] == ['sbh'] or sibling['class'] == ['sbtx1']:
+                            sibling = sibling.find_next_sibling()
+                            continue
+                        # flavor affinities
+                        elif sibling['class'] == ['h4']:
+                            sibling = sibling.find_next_sibling()
+                            while sibling != None:
+                                #c.execute("INSERT INTO matches VALUES ('%s', '', '', '%s', '')" % (i, s))
+                                try:
+                                    if sibling.find_next_sibling()['class'] in (['lh'], ['lh1'], ['p1']):
+                                        break
+                                except:
+                                    break
+                                sibling = sibling.find_next_sibling()
+                
+                        # match-low
+                        elif sibling.find('strong') == None:
+                            addMatches(ingredients, matches, ingredient_ids, id, i, s, 1)
+                        else:
+                            # match-medium
+                            if s.islower():
+                                addMatches(ingredients, matches, ingredient_ids, id, i, s, 2)
+                            # match-high
+                            elif s.isupper() and s.startswith('*') == False:
+                                addMatches(ingredients, matches, ingredient_ids, id, i, s, 3)
+                            # match-holy
+                            elif s.isupper() and s.startswith('*') == True:
+                                addMatches(ingredients, matches, ingredient_ids, id, i, s[1:], 4)
+                
+                        sibling = sibling.find_next_sibling()
+        
+        
+                    print('')
 
+        writeResultsToJSON('ingredients.json', ingredients)
+        writeResultsToJSON('matches.json', matches)
 
-    writeResultsToJSON('ingredients.json', ingredients)
-    writeResultsToJSON('matches.json', matches)
-
-    writeIngredientsToTable(ingredients)
-    writeMatchesToTable(matches)
-
-    conn.commit()
-    conn.close()
+        writeIngredientsToTable(c, ingredients)
+        writeMatchesToTable(c, matches)
+        
+        conn.commit()
+        conn.close()
