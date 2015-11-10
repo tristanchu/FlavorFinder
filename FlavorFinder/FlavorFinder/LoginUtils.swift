d60e6b3aeea7fb433893ee4339ba4c213145c771
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2015-11-10 14:14:11 -0500

    Remove Keychain from app but keep functions for later :dolphin:

--- FlavorFinder/FlavorFinder/LoginUtils.swift
+++ FlavorFinder/FlavorFinder/LoginUtils.swift
@@ -52,30 +52,6 @@ func removeUserSession() -> Void {
     currentUser = nil
 }
 
-/**
-    getUsernameFromKeychain
-
-    Gets username currently stored in the Keychain. If no user stored, gets
-    the default username "default" as String.
-
-    @return: username - String
-*/
-func getUsernameFromKeychain() -> String {
-    return MyKeychainWrapper.myObjectForKey(kSecAttrAccount) as! String
-}
-
-/**
-    getPasswordFromKeychain
-
-    Gets password currently stored in the Keychain. If no user stored, gets
-    the default password "default" as String.
-
-    @return: password - String
-*/
-func getPasswordFromKeychain() -> String {
-    return MyKeychainWrapper.myObjectForKey(kSecValueData) as! String
-}
-
 // ----------------------------------------------------------------------
 // INPUT VALIDATION FUNCTIONS -------------------------------------------
 // ----------------------------------------------------------------------
@@ -115,3 +91,49 @@ func isInvalidPassword(password: String) -> Bool {
     return (password.isEmpty ||
         password.characters.count > PASSWORD_CHAR_MAX ||
         password.characters.count < PASSWORD_CHAR_MIN)}
+
+
+// ----------------------------------------------------------------------
+// KEYCHAIN FUNCTIONS ---------------------------------------------------
+// ----------------------------------------------------------------------
+
+
+// NOTE: Keeping Keychain functions for when we develop persistent data since
+//       now we will need it when dealing with exiting and re-entering the app.
+
+/**
+storeLoginInKeychain
+
+Stores current user's username and password in the Keychain
+
+@param: user - PFUser
+*/
+func storeLoginInKeychain(username: String, password: String) -> Void {
+    MyKeychainWrapper.mySetObject(password, forKey: kSecValueData)
+    MyKeychainWrapper.mySetObject(username, forKey: kSecAttrAccount)
+    MyKeychainWrapper.writeToKeychain()
+}
+
+/**
+getUsernameFromKeychain
+
+Gets username currently stored in the Keychain. If no user stored, gets
+the default username "default" as String.
+
+@return: username - String
+*/
+func getUsernameFromKeychain() -> String {
+    return MyKeychainWrapper.myObjectForKey(kSecAttrAccount) as! String
+}
+
+/**
+getPasswordFromKeychain
+
+Gets password currently stored in the Keychain. If no user stored, gets
+the default password "default" as String.
+
+@return: password - String
+*/
+func getPasswordFromKeychain() -> String {
+    return MyKeychainWrapper.myObjectForKey(kSecValueData) as! String
+}
