80dff6a2cd698826524912864b4802200d17d5b1
jaki1234 <blumoon729@MacBook-Pro-33.local>
2016-01-11 13:47:34 -0500

    container system in storyboard

---
+++ FlavorFinder/FlavorFinder/ContainerViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ContainerViewController.swift
+//  FlavorFinder
+//
+//  Created by Jaki Kimball on 1/11/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//  inspired by the tutorial here: https://kodesnippets.wordpress.com/2015/08/11/container-view-in-ios/
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    
+    var vc : UIViewController!
+    var segueIdentifier : String!
+    var lastViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /// could call a default view with segueIdentifierReceivedFromParent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func segueIdentifierReceivedFromParent(segueIdentifier: String){
+        self.segueIdentifier = segueIdentifier
+        self.performSegueWithIdentifier(self.segueIdentifier, sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == segueIdentifier {
+            // Avoids creation of a stack of view controllers
+            if lastViewController != nil{
+                lastViewController.view.removeFromSuperview()
+            }
+            // Adds View Controller to Container "first" or "second"
+            vc = segue.destinationViewController
+            self.addChildViewController(vc)
+            vc.view.frame = CGRect(x: 0,y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            self.view.addSubview(vc.view)
+            vc.didMoveToParentViewController(self)
+            lastViewController = vc
+        }
+    }
+}
--- FlavorFinder/FlavorFinder/LandingPageController.swift
+++ FlavorFinder/FlavorFinder/LandingPageController.swift
@@ -12,11 +12,17 @@ import Parse
 
 class LandingPageController: UIViewController, UITextFieldDelegate {
     var gotSearch = false
+    let segueToSearchResults = "segueLandingToSearchResults"
     @IBOutlet weak var LandingContainerView: UIView!
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         LandingContainerView.backgroundColor = MATCH_HIGH_COLOR
-        
+        if gotSearch {
+            print("got search")
+            self.performSegueWithIdentifier(segueToSearchResults, sender: nil)
+        } else {
+            print("no search")
+        }
     }
 }
