bed48474df005b590947ad1d0046326bac1c271a
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-01-24 14:28:22 -0500

    created test login button

--- FlavorFinder/FlavorFinder/MorePageController.swift
+++ FlavorFinder/FlavorFinder/MorePageController.swift
@@ -12,4 +12,25 @@ import Parse
 
 class MorePageController: UIViewController, UITextFieldDelegate {
 
+
+    // FOR TEST PURPOSESONLY!!!
+    @IBOutlet weak var morePageLabel: UILabel!
+    @IBAction func loginTestUser(sender: AnyObject) {
+        let username = "testUser";
+        let password = "testUser";
+        // Authenticate user with Parse
+        PFUser.logInWithUsernameInBackground(
+            username, password: password) {
+                (user: PFUser?, error: NSError?) -> Void in
+
+                if user != nil {
+                    // User exists - set user session & go to Match Table
+                    setUserSession(user!)
+
+                    // Show on more page:
+                    self.morePageLabel.text = currentUser?.username;
+                }
+        }
+    }
+
 }
