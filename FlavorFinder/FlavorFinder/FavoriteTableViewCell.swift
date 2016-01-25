593018646d1179eded3e4f1a1ff5263fa5b05070
Courtney Ligh <courtneyligh@cligh.dartmouthsigep.org>
2016-01-25 00:45:39 -0500

    Favs - need pointer value for user

---
+++ FlavorFinder/FlavorFinder/FavoriteTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteTableViewCell.swift
+//  FlavorFinder
+//
+//  Created by Courtney Ligh on 1/24/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+//import UIKit
+//import MGSwipeTableCell
+//
+//class FavoriteTableViewCell: MGSwipeTableCell {
+//    // MARK: Properties
+//    @IBOutlet weak var favoriteCellLabel: UILabel!
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//    
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//        
+//        // Configure the view for the selected state
+//    }
+//    
+//}
--- FlavorFinder/FlavorFinder/FavoritesPageController.swift
+++ FlavorFinder/FlavorFinder/FavoritesPageController.swift
@@ -10,18 +10,110 @@ import Foundation
 import Parse
 import UIKit
 
-class FavoritesPageController : UIViewController, UITextFieldDelegate {
+class FavoritesPageController: UITableViewController {
 
-    @IBOutlet weak var favLabel: UILabel!
+    // MARK: Properties:
+    let favoriteCellName = "FavoriteTableViewCell"  // cell identifier
+    var favoriteCells = [PFIngredient]()    // array of cells to display
+    
+    // Parse related:
+    let ingredientColumn = "ingredient"
+    let favoriteClassName = "Favorite"
+    let userColumnName = "user"
 
+    @IBOutlet var favoritesTableView: UITableView!
+
+
+    // MARK: Override methods: ----------------------------------------------
+    /* viewDidLoad:
+            Additional setup after loading the view (upon open)
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        // Connect table view to this controller (done in storyboard too)
+        favoritesTableView.dataSource = self
+        favoritesTableView.delegate = self
+        favoritesTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: favoriteCellName)
+    }
+
+    /* viewDidAppear:
+            Setup when user goes into page.
+    */
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        // update view here!
 
-        // test
-        favLabel.text = currentUser?.username;
+        // reload table view:
+        populateFavoritesTable()
+        favoritesTableView.reloadData()
+
     }
+
+    /*  tableView -> int
+            returns number of cells to display
+    */
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.favoriteCells.count
+    }
+
+    /* tableView -> UITableViewCell
+            creates cell for each index in favoriteCells
+    */
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(favoriteCellName, forIndexPath: indexPath)
+        cell.textLabel?.text = favoriteCells[indexPath.item].name
+        return cell
+    }
+
+
+    // MARK: Functions -------------------------------------------------
+    
+    /* populateFavoritesTable:
+        clears current favorites array; gets user favorites from parse
+        if user is logged in.
+    
+        TODO: Make a cache of user favs that we update when they log in,
+            favorite, or delete something to limit parse calls.
+    */
+    func populateFavoritesTable(){
+        favoriteCells.removeAll()
+
+        if (currentUser != nil) {
+            // Testing for user:
+            let test4 = PFIngredient(name: currentUser!.username!);
+            favoriteCells.append(test4);
+            
+            // Get user favs from parse if logged in and show:
+            let parseFavorites = getFavoritesFromParse(currentUser!);
+            if !parseFavorites.isEmpty {
+                for fav in parseFavorites {
+                    if let ingredientName: String = fav.objectForKey(ingredientColumn) as? String {
+                        let favIngredient = PFIngredient(name: ingredientName);
+                        favoriteCells.append(favIngredient);
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    /* getFavoritesFromParse:
+        gets the favorites from parse for given user
+    */
+    func getFavoritesFromParse(user: PFUser) -> [PFObject] {
+        let query = PFQuery(className: favoriteClassName)
+        query.whereKey(userColumnName, equalTo: (user.objectId)!) // NEED POINTER VALUE?
+        let resultArray: [PFObject]
+        do {
+            resultArray = try query.findObjects()
+        } catch {
+            resultArray = []
+        }
+        return resultArray;
+    }
+    
 
 
 }
