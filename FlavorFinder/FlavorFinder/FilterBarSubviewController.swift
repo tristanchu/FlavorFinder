e2e85af0ad2569abf53437a00987c6db706285e8
jaki1234 <blumoon729@MacBook-Pro-33.local>
2016-01-31 13:49:55 -0500

    storyboarding and creating new files for contained aspects; update project to 9.2 target

---
+++ FlavorFinder/FlavorFinder/FilterBarSubviewController.swift
@@ -0,0 +1,14 @@
+//
+//  FilterBarSubviewController.swift
+//  FlavorFinder
+//
+//  Created by Jaki Kimball on 1/31/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilterBarSubviewController : UIViewController {
+    
+}
