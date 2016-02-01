3bc3fc72f90cb10f6b3cff960bec5538b43387ed
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-02-01 15:20:00 -0500

    :dart: connected register page ui to code

--- FlavorFinder/FlavorFinder/RegisterView.swift
+++ FlavorFinder/FlavorFinder/RegisterView.swift
@@ -14,13 +14,41 @@ import Parse
 
 class RegisterView : UIViewController, UITextFieldDelegate {
     
-    // MARK: Properties
+    // MARK: Properties -----------------------------------
 
-    
-    
-    
-    
-    
+    // Text Labels:
+    @IBOutlet weak var signUpPromptLabel: UILabel!
+    @IBOutlet weak var warningTextLabel: UILabel!
+
+    // Text Fields:
+    @IBOutlet weak var usernameSignUpField: UITextField!
+    @IBOutlet weak var pwSignUpField: UITextField!
+    @IBOutlet weak var retypePwSignUpField: UITextField!
+    @IBOutlet weak var emailSignUpField: UITextField!
+
+    // Buttons (for UI)
+    @IBOutlet weak var backToLoginButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    let backBtnString =
+        String.fontAwesomeIconWithName(.ChevronLeft) + " Back to login"
+
+    // MARK: Actions -----------------------------------
+    @IBAction func createAccountAction(sender: AnyObject) {
+        print("clicked create account")
+    }
+
+    @IBAction func backToLoginAction(sender: AnyObject) {
+        print("clicked back to login")
+    }
+
+    // MARK: Override Functions --------------------------
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set font awesome chevron:
+        backToLoginButton.setTitle(backBtnString, forState: .Normal)
+    }
     
     
 }
