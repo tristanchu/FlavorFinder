a3071b5cb894893eef3321059bcf150e76d75a29
Courtney Ligh <courtneyligh@cligh.dartmouthsigep.org>
2016-02-01 01:46:27 -0500

    :dog: create login page file

---
+++ FlavorFinder/FlavorFinder/LoginViewPage.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewPage.swift
+//  FlavorFinder
+//
+//  Handles the login view
+//
+//  Created by Courtney Ligh on 2/1/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class LoginViewPage : UIViewController {
+    
+    
+    
+    
+}
