2659b3c50f8a757f9c2561058d6a82943d13dc11
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-02-10 11:51:14 -0500

    :cat: search bar on edit ingredient page shows properly

--- FlavorFinder/FlavorFinder/EditListPage.swift
+++ FlavorFinder/FlavorFinder/EditListPage.swift
@@ -9,13 +9,20 @@
 import UIKit
 import Parse
 
-class EditListPage: UIViewController, UITextFieldDelegate {
+class EditListPage: UIViewController, UITextFieldDelegate,
+    UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Properties:
     var ingredientList = [PFIngredient]()
     var listObject: PFObject!
     var listTitle = ""
-    
+
+    // adding ingredient related:
+    var activeSearch : Bool = false;
+    var allIngredients : [PFIngredient]? // set on load
+    var filteredResults : [PFIngredient] = []
+    let CELL_IDENTIFIER = "searchResultCell"
+
     // Parse related:
     let listClassName = "List"
     let ingredientsColumnName = "ingredients"
@@ -24,19 +31,23 @@ class EditListPage: UIViewController, UITextFieldDelegate {
 
     // Visual related:
     let pageTitle = "Edit List"
-    
+
     // Navigation:
     var backBtn: UIBarButtonItem = UIBarButtonItem()
     let backBtnAction = "backBtnClicked:"
     let backBtnString = String.fontAwesomeIconWithName(.ChevronLeft) + " Back"
-    
+
     // MARK: connections:
     @IBOutlet weak var listNamePromptLabel: UILabel!
     @IBOutlet weak var listTitleLabel: UILabel!
     @IBOutlet weak var newNamePromptLabel: UILabel!
-    
+    @IBOutlet weak var addIngredientPromptLabel: UILabel!
+
     @IBOutlet weak var newNameTextField: UITextField!
-    
+    @IBOutlet weak var addIngredientSearchBar: UISearchBar!
+
+    @IBOutlet weak var searchTableView: UITableView!
+
     @IBAction func submitAction(sender: AnyObject) {
         if newNameTextField.text != nil {
             listObject.setObject(newNameTextField.text!, forKey: ListTitleColumnName)
@@ -44,38 +55,43 @@ class EditListPage: UIViewController, UITextFieldDelegate {
             listObject.saveInBackground()
         }
     }
-    
+
     @IBAction func cancelAction(sender: AnyObject) {
         newNameTextField.text = ""
     }
-    
-    
+
+
     // MARK: Override methods: ----------------------------------------------
-    
-    
+
+
     /* viewDidLoad:
     Additional setup after loading the view (upon open)
     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Set title label to list title:
         listTitleLabel.text = listTitle
 
-        // set up text field 
+        // set up text field
         newNameTextField.delegate = self
         newNameTextField.setTextLeftPadding(5)
+
+        // set up delegates search bar and table view:
+        addIngredientSearchBar.delegate = self
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
 
         // Navigation buttons:
         setUpBackButton()
     }
-    
+
     /* viewDidAppear:
     Setup when user goes into page.
     */
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         // Get navigation bar on top:
         if let navi = self.tabBarController?.navigationController
             as? MainNavigationController {
@@ -87,11 +103,96 @@ class EditListPage: UIViewController, UITextFieldDelegate {
                 self.tabBarController?.navigationItem.title = "\(self.pageTitle)"
                 self.backBtn.enabled = true
         }
+
+        // Hide search bar results on load:
+        searchTableView.hidden = true
     }
-    
+
+    // MARL: UITableViewDataSource protocol functions --------------------
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        activeSearch = true;
+    }
+
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        activeSearch = false;
+        searchTableView.hidden = true
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        activeSearch = false;
+        searchTableView.hidden = true
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        activeSearch = false;
+        searchTableView.hidden = true
+    }
+
+    /* searchBar - for UITableViewDataSource
+    gives table view ingredients filtered by search:
+    */
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        // fills filteredResults based on text entered
+        if let allIngredients = _allIngredients as? [PFIngredient] {
+            filteredResults = allIngredients.filter({ (ingredient) -> Bool in
+                let tmp: PFObject = ingredient
+                let range = tmp[_s_name].rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+                return range.location != NSNotFound
+            })
+            if filteredResults.isEmpty {
+                activeSearch = false
+                searchTableView.hidden = true
+            } else {
+                activeSearch = true
+                searchTableView.hidden = false
+            }
+            self.searchTableView.reloadData()
+        }
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    /* tableView -- UITableViewDelegate func
+    - returns number of rows to display
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(activeSearch) {
+            return filteredResults.count
+        }
+        // have no cells if no search:
+        return 0;
+    }
+
+    /* tableView - UITableViewDelegate func
+    - cell logic
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = searchTableView.dequeueReusableCellWithIdentifier(
+            CELL_IDENTIFIER, forIndexPath: indexPath)
+        // set cell label:
+        cell.textLabel?.text = filteredResults[indexPath.item].name
+        // Give cell a chevron:
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        return cell;
+    }
+
+    /* tableView -> happens on selection of row
+    - sets selected row to current search
+    - goes to search result view
+    */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // make selected row the current search:
+        let selected : PFIngredient = filteredResults[indexPath.row]
+        print("selected \(selected.name)")
+    }
+
+
     // MARK: Back Button Functions -----------------------------------------
-    
-    
+
+
     /* setUpBackButton
     sets up the back button visuals for navigation
     */
@@ -102,7 +203,7 @@ class EditListPage: UIViewController, UITextFieldDelegate {
         backBtn.target = self
         backBtn.action = "backBtnClicked"  // refers to: backBtnClicked()
     }
-    
+
     /* backBtnClicked
     - action for back button
     */
