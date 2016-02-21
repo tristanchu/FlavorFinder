b4182922056901ade3d12b18c5a801945fbea7a7
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-02-21 14:40:48 -0500

    :monkey: connected add ingredient to list page

---
+++ FlavorFinder/FlavorFinder/AddToListPage.swift
@@ -0,0 +1,18 @@
+//
+//  AddToListPage.swift
+//  FlavorFinder
+//
+//  Created by Courtney Ligh on 2/21/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class AddToListPage: SearchIngredientsViewController, UITextFieldDelegate {
+    
+    
+    
+    
+    
+}
