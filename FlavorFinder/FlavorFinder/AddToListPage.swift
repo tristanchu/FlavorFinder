527a268ef2582f3efb8377e1150d4fd1d28df380
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-02-22 13:13:54 -0500

    :frog: addToList page has same add to list functionality as edit page

--- FlavorFinder/FlavorFinder/AddToListPage.swift
+++ FlavorFinder/FlavorFinder/AddToListPage.swift
@@ -16,19 +16,42 @@ class AddToListPage: SearchIngredientsViewController, UITextFieldDelegate {
     var listObject: PFObject!
     var listTitle = ""
     
-    var cancelBtn: UIBarButtonItem = UIBarButtonItem()
-    let cancelBtnAction = "cancelBtnClicked:"
-    let cancelBtnString = String.fontAwesomeIconWithName(.ChevronLeft) + " Cancel"
+    // Parse related:
+    let listClassName = "List"
+    let ingredientsColumnName = "ingredients"
+    let userColumnName = "user"
+    let ListTitleColumnName = "title"
     
     
     // Visual:
     let pageTitle = "Add Ingredient"
+    let addIngredientPrompt = "Add an ingredient to "
+    let alreadyContains = " is already in your list"
+    
+    // Navigation:
+    var cancelBtn: UIBarButtonItem = UIBarButtonItem()
+    let cancelBtnAction = "cancelBtnClicked:"
+    let cancelBtnString = String.fontAwesomeIconWithName(.ChevronLeft) + " Cancel"
+    
+    // Outlets:
+    @IBOutlet weak var addIngredientPromptLabel: UILabel!
+    
+    @IBOutlet weak var addIngredientSearchBar: UISearchBar!
+    @IBOutlet weak var addIngredientTableView: UITableView!
+    
     
     // MARK: Override methods: ------------
     /* viewDidLoad:
     Additional setup after loading the view (upon open)
     */
     override func viewDidLoad() {
+        // Assign values to parent class variables:
+        ingredientSearchBar = addIngredientSearchBar
+        searchTable = addIngredientTableView
+        
+        // make addIngredientPrompt empty until view appears:
+        addIngredientPromptLabel.text = ""
+        
         super.viewDidLoad()
         
         // Navigation Visuals:
@@ -41,6 +64,8 @@ class AddToListPage: SearchIngredientsViewController, UITextFieldDelegate {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
+        // set ingredient prompt to the use list name:
+        addIngredientPromptLabel.text = "\(addIngredientPrompt)\(listTitle)"
         
         // Get navigation bar on top:
         if let navi = self.tabBarController?.navigationController
@@ -75,6 +100,25 @@ class AddToListPage: SearchIngredientsViewController, UITextFieldDelegate {
     */
     func cancelBtnClicked() {
         self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    // MARK: Overriding parent class functions
+    
+    /* gotSelectedIngredient
+    - handle selection of ingredient in search: add to list if not already in list
+    */
+    override func gotSelectedIngredient(selected: PFIngredient) {
+        var userList = listObject.objectForKey(ingredientsColumnName) as! [PFIngredient]
+        if userList.contains(selected) && selected.isDataAvailable() {
+            addIngredientPromptLabel.text = "\(selected.name)\(alreadyContains)"
+        } else {
+            userList.append(selected)
+            listObject.setObject(userList, forKey: ingredientsColumnName)
+            listObject.saveInBackground()
+            if selected.isDataAvailable() {
+                addIngredientPromptLabel.text = "Added \(selected.name)"
+            }
+        }
     }
     
 
