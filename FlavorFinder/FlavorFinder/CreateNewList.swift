7c2b6fdff9d163b99a9d515a206d696beab58bf8
Courtney Ligh <courtneyligh@cligh.wireless.dartmouth.edu>
2016-02-24 20:14:24 -0500

    :palm_tree: Create new list page ui

---
+++ FlavorFinder/FlavorFinder/CreateNewList.swift
@@ -0,0 +1,24 @@
+//
+//  CreateNewList.swift
+//  FlavorFinder
+//
+//  Created by Courtney Ligh on 2/24/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class CreateNewList: UIViewController {
+    
+    // MARK: Properties:
+    var listObject: PFObject!
+    var listTitle = "Untitled"
+    
+    
+    
+    
+    
+    
+    
+}
