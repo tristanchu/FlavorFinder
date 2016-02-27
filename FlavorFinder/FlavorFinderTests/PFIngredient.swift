b5849562c9b20416d09a82666b7ca3a77249a96a
jon <other@dhcp-210-77.lab.cs.dartmouth.edu>
2016-02-27 00:09:00 -0500

    Updated database functions for user adding ingredient/match.

--- FlavorFinder/FlavorFinderTests/PFIngredient.swift
+++ FlavorFinder/FlavorFinderTests/PFIngredient.swift
@@ -12,10 +12,16 @@ import Parse
 class PFIngredient : PFObject, PFSubclassing {
     
     @NSManaged var name: String
+    @NSManaged var isDairy: Bool
+    @NSManaged var isNuts: Bool
+    @NSManaged var isVege: Bool
     
-    convenience init(name: String) {
+    convenience init(name: String, isDairy: Bool, isNuts: Bool, isVege: Bool) {
         self.init()
         self.name = name
+        self.isDairy = isDairy
+        self.isNuts = isNuts
+        self.isVege = isVege
     }
     
     override class func initialize() {
