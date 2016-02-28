795d0c8233de4bf2e00ea42af86631c40f286da0
jaki1234 <blumoon729@MacBook-Pro-33.local>
2016-02-28 00:34:31 -0500

    unconstrained UI for add match

---
+++ FlavorFinder/FlavorFinder/AddMatchViewController.swift
@@ -0,0 +1,86 @@
+//
+//  AddMatchViewController.swift
+//  FlavorFinder
+//
+//  Created by Jaki Kimball on 2/27/16.
+//  Copyright © 2016 TeamFive. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddMatchViewController : SearchIngredientsViewController {
+    
+    // MARK: Properties:
+    let pageTitle = "Add A New Match"
+    let alreadyExistsText = " is already a match!"
+    let defaultPrompt = "Which ingredient are you creating a match for?"
+    var firstIngredient : PFIngredient?
+    var secondIngredient : PFIngredient?
+    
+    // MARK: connections:
+    @IBOutlet weak var chooseIngredientSearchBar: UISearchBar!
+    @IBOutlet weak var searchTableView: UITableView!
+    @IBOutlet weak var promptLabel: UILabel!
+    
+    // MARK: Override methods: ----------------------------------------------
+    
+    /* viewDidLoad:
+    */
+    override func viewDidLoad() {
+        // Assign values to parent class variables
+        ingredientSearchBar = chooseIngredientSearchBar
+        searchTable = searchTableView
+        navTitle = pageTitle
+        
+        // Call super
+        super.viewDidLoad()
+    }
+    
+    /* viewDidAppear:
+    */
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        reset()
+    }
+    
+    // MARK: Overriding parent class functions ------------------------------
+    
+    /* gotSelectedIngredient
+    - handle selection of ingredient w.r.t. creating a new match
+    */
+    override func gotSelectedIngredient(selected: PFIngredient) {
+        
+        if currentUser == nil {
+            print("ERROR: Cannot propose a match without being logged in!")
+            return
+        }
+        
+        if let _ = firstIngredient { // try to create a match
+            secondIngredient = selected
+            if let _ = getMatchForTwoIngredients(firstIngredient!, secondIngredient: secondIngredient!) {
+                print("the match already exists")
+            } else {
+                addMatch(currentUser!, firstIngredient: firstIngredient!, secondIngredient: secondIngredient!)
+                print("match added")
+                reset()
+            }
+        } else {
+            // indicate selection
+            firstIngredient = selected
+            print((firstIngredient!))
+        }
+    }
+    
+    // MARK: Other functions ---------------------------------------------------
+    
+    /* reset
+    - resets the view after a match has been added
+    */
+    func reset() {
+        promptLabel.text = defaultPrompt
+        firstIngredient = nil
+        secondIngredient = nil
+    }
+    
+}
